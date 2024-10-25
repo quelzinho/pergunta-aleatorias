7917330184b9b04e7a3f79a7c6c11fdd31e5e0b3
quelzinho <133047355+quelzinho@users.noreply.github.com>
2024-10-25 11:01:03 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -18,6 +18,36 @@ criaCartao(
 
 criaCartao(
     'Línguas',
-    'Lingua mais falada do mundo',
+    'Lingua mais falada do mundo?',
     'Mandarim'
+)
+
+criaCartao(
+    'Carro',
+    'Carro com mais cavalo no mundo?',
+    'O primeiro carro da lista é o Koenigsegg Jesko Absolut. Equipado com motor 5.0 V8 biturbo capaz de entregar impressionantes 1.600 cv e 152,92 kgfm'
+)
+
+criaCartao(
+    'Futebol',
+    'Melhor lateral direito do mundo?',
+    'Trent Alexander Arnold - Liverpool'
+)
+
+criaCartao(
+    'Futebol',
+    'Maior lateral esquerda do mundo?',
+    'Andrew Robertson - Liverpool '
+)
+
+criaCartao(
+    'Vida marinha',
+    'Peixe mais venenoso do mundo?',
+    'Acredita-se que a Chironex fleckeri, também conhecida como box jellyfish ou vespa-do-mar, seja a criatura mais venenosa do mundo, observa o ADW. Sua picada é extremamente mortal para os seres humanos.'
+)
+
+criaCartao(
+    'Animal',
+    'Qual é o animal mais rápido do mundo no ar?',
+    'Surpreendendo a muitos, o animal mais rápido do mundo é o falcão-peregrino. A ave de rapina pode chegar a 320 km/h — quase a velocidade de um carro de Fórmula 1'
 )
